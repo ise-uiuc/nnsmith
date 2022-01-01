cc1f5fd85c37813513f45c57710779304be0e6a6
Jinkun Lin <lazycal12@gmail.com>
2022-01-01 23:56:21 +0000

    trim code

--- nnsmith/input_gen.py
+++ nnsmith/input_gen.py
@@ -57,120 +57,18 @@ class InputGenBase:
     MAX_TRIALS = 2
     DEFAULT_RNG = (0, 1)
 
-    # overload me; can return stats for further analysis
-    # def gen_inputs(self, model, num_inputs, model_path) -> Dict:
-    #     raise NotImplementedError
-
     # overload me; return valid ranges for further analysis
-
     def infer_domain(self, model: onnx.ModelProto) -> List[Range]:
         raise NotImplementedError
-
-    # @classmethod
-    # def _gen_inputs(cls, model, num_inputs, model_path, rngs=None, max_trials=MAX_TRIALS):
-    #     from nnsmith.backends.ort_graph import ORTExecutor
-    #     rngs = rngs or [cls.DEFAULT_RNG]
-    #     rf_exe = ORTExecutor(opt_level=0)  # reference model
-    #     inp_spec = DiffTestBackend.analyze_onnx_io(model)[0]
-    #     trials, succ = 0, 0
-    #     for i in tqdm(range(num_inputs)):
-    #         for ntrials in range(max_trials):
-    #             rng_idx = np.random.randint(len(rngs))
-    #             inp = gen_one_input(
-    #                 inp_spec, l=rngs[rng_idx][0], r=rngs[rng_idx][1])
-    #             out = rf_exe.predict(model, inp)
-    #             if not has_nan(out):
-    #                 break
-    #         trials += ntrials + 1
-    #         succ += not has_nan(out)
-    #         pickle.dump(inp, (Path(model_path) / f'input.{i}.pkl').open("wb"))
-
-    #     print('succ rate:', np.mean(succ / trials))
-    #     return {'succ': succ, 'trials': trials, 'succ_rate': succ / trials}
 
 
 class InputGenV1(InputGenBase):
     def __init__(self) -> None:
         super().__init__()
 
-    # def gen_inputs(self, *args, **kwargs):
-    #     return super()._gen_inputs(*args, **kwargs)
-
     def infer_domain(self, model):
         return [(0, 1)]
 
-
-# class InputGenV2(InputGenBase):
-#     L = -1e2
-#     R = 1e2
-#     EPS = 1e-3
-#     THRES = 1
-
-#     def __init__(self, max_rng_trials=3) -> None:
-#         super().__init__()
-#         self.max_rng_trials = max_rng_trials
-
-#     def _get_range(self, model):
-#         def check(l, r):
-#             succ = 0
-#             for ntrials in range(self.max_rng_trials):
-#                 inp = self.gen_one_input(inp_spec, l, r)
-#                 out = rf_exe.predict(model, inp)
-#                 succ += not self.has_nan(out)
-#             return succ / (ntrials + 1) >= self.THRES
-
-#         rf_exe = ORTExecutor(opt_level=0)  # reference model
-#         inp_spec = DiffTestBackend.analyze_onnx_io(model)[0]
-
-#         a = np.linspace(-1, 1, 10)
-#         # print('inital serach points=', a)
-
-#         def binary_search(l, r, checker):
-#             while r - l > self.EPS:
-#                 mid = (l + r) / 2
-#                 if checker(mid):
-#                     r = mid
-#                 else:
-#                     l = mid
-#             return r
-
-#         valid_rngs = []
-#         for valid_point in a:
-#             included = False
-#             for rng in valid_rngs:
-#                 if rng[0] <= valid_point <= rng[1]:
-#                     included = True
-#             if included or not check(valid_point, valid_point):
-#                 continue
-#             l = binary_search(self.L, valid_point,
-#                               lambda l: check(l, valid_point))
-#             r = -binary_search(-self.R, -valid_point, lambda r: check(l, -r))
-#             # fix boundary to avoid redundant binary search
-#             # [0, 0.99999] -> [0, 1] if 1 is valid, thus saving binary search on 1
-#             for v2 in a:
-#                 if abs(l - v2) <= self.EPS:
-#                     l = min(l, v2)
-#                 if abs(r - v2) <= self.EPS:
-#                     r = max(r, v2)
-#             valid_rngs.append((l, r))
-
-#         if len(valid_rngs) == 0:
-#             print('no valid range found!!!')
-#             valid_rngs = None
-#         return valid_rngs
-
-#     def gen_inputs(self, model, num_inputs, model_path):
-#         st = time.time()
-#         rngs = self._get_range(model)
-#         get_range_time = time.time() - st
-#         st = time.time()
-#         stats = self._gen_inputs(model, num_inputs, model_path, rngs)
-#         gen_input_time = time.time() - st
-#         print('get_range_time=', get_range_time, 'gen_input_time=',
-#               gen_input_time, 'valid ranges=', rngs)
-#         stats['get_range_time'] = get_range_time
-#         stats['gen_input_time'] = gen_input_time
-#         return stats
 
 class NaNChecker:
     def load_model(self, model):
@@ -287,22 +185,6 @@ class InputGenV3(InputGenBase):
             valid_rngs = None
         return valid_rngs
 
-    # def gen_inputs(self, model, num_inputs, model_path):
-    #     self.nan_checker.load_model(model)
-    #     st = time.time()
-    #     rngs = self.infer_domain(model)
-    #     get_range_time = time.time() - st
-
-    #     st = time.time()
-    #     stats = self._gen_inputs(model, num_inputs, model_path, rngs)
-    #     gen_input_time = time.time() - st
-
-    #     # print('get_range_time=', get_range_time, 'gen_input_time=',
-    #     #       gen_input_time, 'valid ranges=', rngs)
-    #     stats['get_range_time'] = get_range_time
-    #     stats['gen_input_time'] = gen_input_time
-    #     return stats
-
     def infer_domain(self, model):
         self.nan_checker.load_model(model)
         if self.nan_checker.no_nan(0, 1):
@@ -311,32 +193,6 @@ class InputGenV3(InputGenBase):
             rngs = self._get_range()
 
         return rngs
-
-
-# def gen_inputs_for_one(num_inputs, model, input_gen: InputGenBase = InputGenV1()):
-#     st = time.time()
-#     stats = input_gen.gen_inputs(
-#         DiffTestBackend.get_onnx_proto(str(model)), num_inputs, Path(model).parent)
-#     stats.update({
-#         'model': model,
-#         'elpased_time': time.time() - st}
-#     )
-#     return stats
-
-
-# def gen_inputs_for_all(root, num_inputs=2, models=None, input_gen: InputGenBase = InputGenV1()):
-#     import pandas as pd
-#     profile = []
-#     models = models or sorted(list(Path(root).glob('model_input/*/*.onnx')))
-#     for model in tqdm(models):
-#         print(model)
-#         profile.append(gen_inputs_for_one(num_inputs, model, input_gen))
-#     # profile = pd.DataFrame(
-#         # profile, columns=['model', 'succ', 'trials', 'succ_rate', 'elapsed_time'])
-#     profile = pd.DataFrame(profile)
-#     profile = profile.sort_values(by=['succ_rate'], ascending=True)
-#     profile.to_pickle(Path(root) / 'gen_profile.pkl')
-#     profile.to_csv(Path(root) / 'gen_profile.csv')
 
 
 def gen_one_input_for_model(model: Union[str, onnx.GraphProto], input_gen: InputGenBase = InputGenV3(), seed=None):
