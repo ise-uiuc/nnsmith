6f7c1758e8b6a38a6d1dc69cf17d3e736d6f1791
ganler <jaway.liu@gmail.com>
2022-03-12 00:30:57 -0600

    light-weight batch execution for coverage evaluation

---
+++ experiments/batch_eval.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import random
+import numpy as np
+
+from nnsmith.backends import DiffTestBackend
+from nnsmith.input_gen import gen_one_input
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--models', nargs='+', required=True, help='List to ONNX model paths')
+    parser.add_argument('--backend', type=str, default='tvm', help='One of ort, trt, tvm, and xla')
+    parser.add_argument('--dev', type=str, default='cpu', help='cpu/gpu')
+    parser.add_argument('--seed', type=int, default=233, help='to generate random input data')
+    args = parser.parse_args()
+
+    # Set global seed
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+
+    backend = None
+    if args.backend == 'tvm':
+        from nnsmith.backends.tvm_graph import TVMExecutor
+        backend = TVMExecutor(opt_level=4)
+    elif args.backend == 'ort':
+        from nnsmith.backends.ort_graph import ORTExecutor
+        backend = ORTExecutor(opt_level=3)
+    else:
+        raise NotImplementedError("Other backends not supported yet.")
+
+    for path in args.models:
+        onnx_model = DiffTestBackend.get_onnx_proto(path)
+        is_diff_test = os.path.exists(path + '.inp.pkl') # TODO: Check if needs to run diff test
+        
+        try:
+            if is_diff_test:
+                # path + '.inp.pkl' -> input tensor dictionary
+                # path + '.out.pkl' -> output tensor dictionary (from PyTorch)
+                # Run diff test to verify.
+                pass # TODO: Implement diff test
+            else:
+                input_spec, onames = DiffTestBackend.analyze_onnx_io(onnx_model)
+                eval_inputs = gen_one_input(input_spec, 0, 1)
+                backend.predict(onnx_model, eval_inputs)
+        except Exception as e:
+            print("==============================================================", file=sys.stderr)
+            print(f"Failed execution at {path}", file=sys.stderr)
+            print(e, file=sys.stderr)
+            # Done!
