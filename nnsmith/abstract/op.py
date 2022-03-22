fix: softmax

nnsmith/abstract/op.py
@@ -1122,7 +1122,7 @@ class Softmax(ElementWiseUnaryOp):
     def _requires(self, input_shapes: List[ShapeVar]) -> List[z3.ExprRef]:
         return [
             nnsmith_lt(self.dim, input_shapes[0].ndims),
-            nnsmith_ge(self.dim, 1)]
+            nnsmith_ge(self.dim, 0)]
 
     def torch(self) -> Callable[..., torch.Tensor]:
         return torch.nn.Softmax(dim=self.dim)