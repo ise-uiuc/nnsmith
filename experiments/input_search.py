1ffa6a8e052b2519b812feb8d089dcbd94312437
Jinkun Lin <lazycal12@gmail.com>
2022-04-19 20:22:41 +0000

    Generate models first to avoid interference.

--- experiments/input_search.py
+++ experiments/input_search.py
@@ -2,6 +2,9 @@
 simply generate a bunch of models and see if the can find viable inputs.
 """
 
+from multiprocessing import Process
+import shutil
+import uuid
 from nnsmith.graph_gen import random_model_gen, SymbolNet
 from nnsmith.dtype_test import rewrite_op_dtype
 from nnsmith.abstract.op import ALL_OP_TYPES
@@ -21,6 +24,55 @@ import cloudpickle
 import pickle
 import networkx as nx
 
+
+def mknet(args, differentiable_ops):
+    model_seed = random.getrandbits(32)
+    gen, solution = random_model_gen(
+        mode=args.mode, min_dims=args.min_dims, seed=model_seed, max_nodes=args.max_nodes,
+        timeout=args.timeout, candidates_overwrite=differentiable_ops, init_fp=True)
+    gen.viz('debug.png')
+    net = SymbolNet(gen.abstract_graph, solution, verbose=args.verbose,
+                    alive_shapes=gen.alive_shapes)
+    net.eval()
+    return net, gen.num_op(), model_seed
+
+
+def mknets(args):
+    model_path = os.path.join(args.root, 'model')
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+    __DIFF_CACHE__ = 'config/diff.pkl'
+    differentiable_ops = rewrite_op_dtype(
+        ALL_OP_TYPES, backend=None, diff=True, verbose=True, cache=__DIFF_CACHE__)
+    print(differentiable_ops)
+    results = {
+        'model_seed': [],
+        'n_nodes': [],
+    }
+    for model_id in tqdm(range(args.n_model)):
+        while True:
+            net, num_op, model_seed = mknet(args, differentiable_ops)
+            # break # NOTE: uncomment this line to see how serious the issue is.
+            if net.n_vulnerable_op > 0:
+                break
+        try:
+            torch2onnx(net, os.path.join(
+                model_path, f'{model_id}.onnx'))
+        except Exception as e:
+            print(e)
+            print('Failed to convert to onnx')
+        if hasattr(net, 'graph'):
+            nx.drawing.nx_pydot.to_pydot(net.graph).write_png(os.path.join(
+                model_path, f'{model_id}-graph.png'))
+            net.to_picklable()
+        cloudpickle.dump(net, open(os.path.join(
+            model_path, f'{model_id}-net.pkl'), 'wb'), protocol=4)
+        results['n_nodes'].append(num_op)
+        results['model_seed'].append(model_seed)
+    df = pd.DataFrame(results)
+    df.to_csv(os.path.join(args.root, "model_info.csv"), index=False)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--max_nodes', type=int, default=25)
@@ -33,32 +85,36 @@ if __name__ == '__main__':
     parser.add_argument('--verbose', action='store_true')
     parser.add_argument('--output_path', type=str, default='output.onnx')
     parser.add_argument('--mode', type=str, default='random')
-    parser.add_argument('--save_model', type=str,
-                        help='save models to this path')
+    parser.add_argument('--root', type=str,
+                        help='save models and results to this path')
     # for reproducibility
     parser.add_argument(
-        '--load_model', help='Use saved models from specified path')
+        '--load', help='Use saved models from specified path passed to --root')
     args = parser.parse_args()
 
-    if args.save_model is not None:
-        mkdir(args.save_model)
-    if args.load_model is not None:
-        ref_df = pd.read_csv(os.path.join(args.load_model, 'model_info.csv'))
-    else:
-        ref_df = None
-    __DIFF_CACHE__ = 'config/diff.pkl'
-    differentiable_ops = rewrite_op_dtype(
-        ALL_OP_TYPES, backend=None, diff=True, verbose=True, cache=__DIFF_CACHE__)
-    print(differentiable_ops)
+    del_root = False
+    if args.root is None:
+        args.root = 'input_search_root_' + str(uuid.uuid4())
+        del_root = True
+    mkdir(args.root)
 
     exp_seed = args.exp_seed
     if exp_seed is None:
         exp_seed = random.getrandbits(32)
     print(f"Using seed {exp_seed}")
-
     np.random.seed(exp_seed)
     random.seed(exp_seed)
     torch.manual_seed(exp_seed)
+
+    # generate models
+    if args.load is None:
+        p = Process(target=mknets, args=(args,))
+        p.start()
+        p.wait()
+        args.load = args.root
+
+    ref_df = pd.read_csv(os.path.join(
+        args.load, 'model_info.csv'))  # load models
 
     exp_name = f'r{exp_seed}-model{args.n_model}-node{args.max_nodes}-inp-search.csv'
     results = {
@@ -74,45 +130,13 @@ if __name__ == '__main__':
         'grad-succ': [],
     }
 
-    def mknet():
-        model_seed = random.getrandbits(32)
-        gen, solution = random_model_gen(
-            mode=args.mode, min_dims=args.min_dims, seed=model_seed, max_nodes=args.max_nodes,
-            timeout=args.timeout, candidates_overwrite=differentiable_ops, init_fp=True)
-        gen.viz('debug.png')
-        net = SymbolNet(gen.abstract_graph, solution, verbose=args.verbose,
-                        alive_shapes=gen.alive_shapes)
-        net.eval()
-        return net, gen.num_op(), model_seed
-
     for model_id in tqdm(range(args.n_model)):
-        while True:
-            if ref_df is None:
-                net, num_op, model_seed = mknet()
-            else:
-                model_seed = ref_df['model_seed'][model_id]
-                net = pickle.load(
-                    open(os.path.join(args.load_model, f'{model_id}-net.pkl'), 'rb'))
-                net.use_gradient = False
-                num_op = ref_df['n_nodes'][model_id]
-            # break # NOTE: uncomment this line to see how serious the issue is.
-            if net.n_vulnerable_op > 0:
-                break
+        model_seed = ref_df['model_seed'][model_id]
+        net = pickle.load(
+            open(os.path.join(args.load, f'model/{model_id}-net.pkl'), 'rb'))
+        net.use_gradient = False
+        num_op = ref_df['n_nodes'][model_id]
         print('model_seed=', model_seed)
-
-        if args.save_model is not None:  # save the original model before modified by grad-search
-            try:
-                torch2onnx(net, os.path.join(
-                    args.save_model, f'{model_id}.onnx'))
-            except Exception as e:
-                print(e)
-                print('Failed to convert to onnx')
-            if hasattr(net, 'graph'):
-                nx.drawing.nx_pydot.to_pydot(net.graph).write_png(os.path.join(
-                    args.save_model, f'{model_id}-graph.png'))
-                net.to_picklable()
-            cloudpickle.dump(net, open(os.path.join(
-                args.save_model, f'{model_id}-net.pkl'), 'wb'), protocol=4)
 
         results['n_nodes'].append(num_op)
         results['model_seed'].append(model_seed)
@@ -187,9 +211,11 @@ if __name__ == '__main__':
 
     df = pd.DataFrame(results)
     df.to_csv(exp_name, index=False)
-    if args.save_model is not None:
+    if args.root is not None:
         os.system(
-            f'cp {exp_name} {os.path.join(args.save_model, "model_info.csv")}')
-        with open(os.path.join(args.save_model, 'stats.log'), 'w') as f:
+            f'cp {exp_name} {os.path.join(args.root, "model_info.csv")}')
+        with open(os.path.join(args.root, 'stats.log'), 'w') as f:
             f.write(str(df.mean()) + '\n')
     print(df.mean())
+    if del_root:
+        shutil.rmtree(args.root)
