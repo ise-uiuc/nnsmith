55a5493b48cf6c8777952c48870c3f56b0a161d3
Jinkun Lin <lazycal12@gmail.com>
2022-06-11 15:16:03 +0000

    Delete unused files.

--- tests/test_graph_input_gen.py
+++
@@ -1,8 +0,0 @@
-from nnsmith.graph_input_gen import gen_model_and_range_safe
-
-
-if __name__ == '__main__':
-    for i in range(5):
-        print(gen_model_and_range_safe(
-            './output.onnx', seed=i, max_node_size=20, max_gen_millisec=10)[1:])
-    print('pass')
